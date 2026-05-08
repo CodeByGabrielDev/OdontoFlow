ab1ea9d07a41d48f001a3644cc378543f6b53de4
Gabriel Lima de Oliveira <ogabriellima1999@gmail.com>
2026-05-07 23:44:07 -0300

    mainPage

---
+++ odontoflow/odontoflow/src/main/java/com/odontoflow/odontoflow/Services/PrincipalPageService.java
@@ -0,0 +1,18 @@
+package com.odontoflow.odontoflow.Services;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalPageService {
+
+    private void registerProfessional() {
+
+    }
+
+    private void loginProfessional() {
+
+    }
+}
